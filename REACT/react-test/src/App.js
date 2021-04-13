30601e1d97fff9842382625901c426e01a242b00
AleFeri <50620354+AleFeri@users.noreply.github.com>
2021-04-13 11:15:42 +0200

    added simple Home Page

--- REACT/react-test/src/App.js
+++ REACT/react-test/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Shop from './Shop/Shop';
 import DettaglioProdotto from './DettaglioProdotto/DettaglioProdotto';
 import Nav from './Nav/Nav';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home/Home';
 
 function App() {
 	return (
 		<Router>
 			<Nav></Nav>
 			<Switch>
-				<Route path="/" exact component={Shop} />
+				<Route path="/" exact component={Home} />
 				<Route path="/shop" exact component={Shop} />
 				<Route path="/shop/:id" exact component={DettaglioProdotto} />
 			</Switch>
