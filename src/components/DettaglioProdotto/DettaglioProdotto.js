2f25dd5f47e9b4cd1e41ada3ff07f37b2749bf3d
riccardodeluca <xdelu02@gmail.com>
2021-04-27 12:17:59 +0200

    added dettaglioprodotto
    Co-authored-by: AleFeri <AleFeri@users.noreply.github.com>

--- src/components/DettaglioProdotto/DettaglioProdotto.js
+++ src/components/DettaglioProdotto/DettaglioProdotto.js
@@ -1,93 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import ReactHtmlParser from 'react-html-parser';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCartArrowDown} from '@fortawesome/free-solid-svg-icons';
+import {addToCart} from '../../actions';
 import './DettaglioProdotto.scss';
 
+function DettaglioProdotto(props) {
+	const [prodotto, setProdotto] = useState({ nome: 'logo' });
+	const [qtaMAX, setQtaMAX] = useState(1);
+	const [qta, setQta] = useState(1);
+	const dispatch = useDispatch();
 
-class DettaglioProdotto extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			props: props,
-			error: null,
-			isLoaded: false,
-			prodotto: {
-				nome: 'logo'
-			},
-			qtaMAX: 1,
-			qta: 1
-		};
-	}
-
-	componentDidMount() {
-		fetch('https://ecommerce.ideeinbit.it/api/prodotti/' + this.state.props.match.params.id)
+	useEffect(() => {
+		fetch('https://ecommerce.ideeinbit.it/api/prodotti/' + props.match.params.id)
 			.then((res) => res.json())
 			.then(
 				(result) => {
 					if (result.quantita === '0') window.location.href = '/shop';
-					this.setState({
-						prodotto: result,
-						qtaMAX: Math.floor(result.quantita === '1' ? 1 : result.quantita / 2 > 10 ? 10 : result.quantita / 2)
-					});
+					setProdotto(
+						result
+					);
+					setQtaMAX(
+						Math.floor(result.quantita === '1' ? 1 : result.quantita / 2 > 10 ? 10 : result.quantita / 2)
+					);
 				},
 				(error) => {
 					console.log(error);
 				}
 			);
-	}
+	}, []);
 
-	handleChange = async (e) => {
-		this.setState({
-			qta: e.target.value
-		});
+	const handleChange = async (e) => {
+		setQta(
+			e.target.value
+		);
 	};
 
-	handleAddToCart(prod) {
-		//useDispatch(addToCart(prod));
-		console.log(prod);
-	};
-
-	render() {
-		return (
-			<div className="dettaglioprodotto flex" id={this.state.prodotto.idProdotto}>
-				<div className="section1">
-					<div>
-						<img src={'https://ecommerce.ideeinbit.it/img/' + this.state.prodotto.nome + '.png'} alt="prodotto" />
-					</div>
-					<div className="details">
-						<h1 className="titolo m">{this.state.prodotto.nome}</h1>
-						<h2 className="prezzo m">€ {this.state.prodotto.prezzo}</h2>
-						<p className="descS m">{this.state.prodotto.descS}</p>
-						<div className="custom-select m">
-							<select onChange={this.handleChange}>
-								{Array.from(new Array(this.state.qtaMAX), (x, i) => i + 1).map((n) => (
-									<option value={n} key={n}>
-										{n}
-									</option>
-								))}
-							</select>
-						</div>
-						<button
-							className="btn-addtocart"
-							onClick={() =>
-								this.handleAddToCart({
-									idProdotto: this.state.prodotto.idProdotto,
-									quantita: this.state.qta
-								})
-							}
-						>
-							ADD TO CHART <FontAwesomeIcon className="fa" icon={faCartArrowDown} aria-hidden="true"></FontAwesomeIcon>
-						</button>
-					</div>
+	return (
+		<div className="dettaglioprodotto flex" id={prodotto.idProdotto}>
+			<div className="section1">
+				<div>
+					<img src={'https://ecommerce.ideeinbit.it/img/' + prodotto.nome + '.png'} alt="prodotto" />
 				</div>
-				<div className="section2">
-					<h3>Descrizione</h3>
-					<p>{ReactHtmlParser(this.state.prodotto.descL)}</p>
+				<div className="details">
+					<h1 className="titolo m">{prodotto.nome}</h1>
+					<h2 className="prezzo m">€ {prodotto.prezzo}</h2>
+					<p className="descS m">{prodotto.descS}</p>
+					<div className="custom-select m">
+						<select onChange={handleChange}>
+							{Array.from(new Array(qtaMAX), (x, i) => i + 1).map((n) => (
+								<option value={n} key={n}>
+									{n}
+								</option>
+							))}
+						</select>
+					</div>
+					<button
+						className="btn-addtocart"
+						onClick={() =>
+							dispatch(addToCart({
+								idProdotto: prodotto.idProdotto,
+								quantita: qta
+							}))
+						}
+					>
+						ADD TO CHART <FontAwesomeIcon className="fa" icon={faCartArrowDown} aria-hidden="true"></FontAwesomeIcon>
+					</button>
 				</div>
 			</div>
-		);
-	}
+			<div className="section2">
+				<h3>Descrizione</h3>
+				<p>{ReactHtmlParser(prodotto.descL)}</p>
+			</div>
+		</div>
+	);
 }
 
 export default DettaglioProdotto;
+ 
