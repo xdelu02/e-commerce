48ca77bcb5d672865af6af94acab8b83ebcb5a21
AleFeri <50620354+AleFeri@users.noreply.github.com>
2021-04-28 18:21:59 +0200

    aggiunta funzionalità al bottone nello shop

--- src/components/Shop/Prodotto/Prodotto.js
+++ src/components/Shop/Prodotto/Prodotto.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import {Link, useHistory} from 'react-router-dom';
+import { addToCart } from '../../../actions';
 import './Prodotto.scss';
 
 function Prodotto(props) {
+	const dispatch = useDispatch();
+	let history = useHistory();
+
+	const handleOnClick = () => {
+		dispatch(
+			addToCart({
+				idProdotto: props.id,
+				quantita: 1
+			})
+		);
+		history.push('/shop');
+	}
+
 	return (
 		<Link className="product-wrapper" to={'/shop/' + props.id}>
 			<img src={props.path} alt="prodotto" className="product-image" />
 			<p className="title">{props.titolo}</p>
 			<p className="descS">{props.descS}</p>
 			<p className="price">{props.prezzo} €</p>
-			<button type="button" className="btn btn--block btn--down-br">
+			<button type="button" className="btn btn--block btn--down-br" onClick={handleOnClick}>
 				ADD TO CART
 			</button>
 		</Link>
