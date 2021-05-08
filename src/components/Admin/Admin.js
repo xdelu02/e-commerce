9c8e0293c1782d0f96aefd90f08a788c2763f445
Riccardo De Luca <xdelu02@gmail.com>
2021-05-09 00:57:13 +0200

    admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,158 +1,36 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import ReceiptIcon from '@material-ui/icons/Receipt';
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import CSSModules from 'react-css-modules';
+import styles from './Admin.module.scss';
+import Logo from '../../assets/logo/logo.png';
+import ordersIcon from '../../assets/icons/piggy-bank.png';
+import profitIcon from '../../assets/icons/check-list.png';
 
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-	root: {
-		display: 'flex'
-	},
-	drawer: {
-		[theme.breakpoints.up('sm')]: {
-			width: drawerWidth,
-			flexShrink: 0
-		}
-	},
-	appBar: {
-		[theme.breakpoints.up('sm')]: {
-			width: `calc(100% - ${drawerWidth}px)`,
-			marginLeft: drawerWidth
-		}
-	},
-	menuButton: {
-		marginRight: theme.spacing(2),
-		[theme.breakpoints.up('sm')]: {
-			display: 'none'
-		}
-	},
-	// necessary for content to be below app bar
-	toolbar: theme.mixins.toolbar,
-	drawerPaper: {
-		width: drawerWidth
-	},
-	content: {
-		flexGrow: 1,
-		padding: theme.spacing(3)
-	}
-}));
-
-function Admin(props) {
-	const { window } = props;
-	const classes = useStyles();
-	const theme = useTheme();
-	const [mobileOpen, setMobileOpen] = React.useState(false);
-
-	const handleDrawerToggle = () => {
-		setMobileOpen(!mobileOpen);
-	};
-
-	const drawer = (
-		<div>
-			<div className={classes.toolbar} />
-
-			<List>
-				<ListItem button key={'Dashboard'}>
-					<ListItemIcon>
-						<DashboardIcon />
-					</ListItemIcon>
-					<ListItemText primary={'Dashboard'} />
-				</ListItem>
-
-				<ListItem button key={'Ordini'}>
-					<ListItemIcon>
-						<ReceiptIcon />
-					</ListItemIcon>
-					<ListItemText primary={'Ordini'} />
-				</ListItem>
-
-				<ListItem button key={'Prodotti'}>
-					<ListItemIcon>
-						<DashboardIcon />
-					</ListItemIcon>
-					<ListItemText primary={'Prodotti'} />
-				</ListItem>
-
-				<ListItem button key={'Admin'}>
-					<ListItemIcon>
-						<SupervisorAccountIcon />
-					</ListItemIcon>
-					<ListItemText primary={'Admin'} />
-				</ListItem>
-			</List>
-		</div>
-	);
-
-	const container = window !== undefined ? () => window().document.body : undefined;
+function Admin() {
+	const [show, setShow] = useState(false);
 
 	return (
-		<div className={classes.root}>
-			<CssBaseline />
-			<AppBar position='fixed' className={classes.appBar}>
-				<Toolbar>
-					<IconButton color='inherit' aria-label='open drawer' edge='start' onClick={handleDrawerToggle} className={classes.menuButton}>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant='h6' noWrap>
-						Gestione shop
-					</Typography>
-				</Toolbar>
-			</AppBar>
-			<nav className={classes.drawer} aria-label='mailbox folders'>
-				{/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-				<Hidden smUp implementation='css'>
-					<Drawer
-						container={container}
-						variant='temporary'
-						anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-						open={mobileOpen}
-						onClose={handleDrawerToggle}
-						classes={{
-							paper: classes.drawerPaper
-						}}
-						ModalProps={{
-							keepMounted: true // Better open performance on mobile.
-						}}
-					>
-						{drawer}
-					</Drawer>
-				</Hidden>
-				<Hidden xsDown implementation='css'>
-					<Drawer
-						classes={{
-							paper: classes.drawerPaper
-						}}
-						variant='permanent'
-						open
-					>
-						{drawer}
-					</Drawer>
-				</Hidden>
-			</nav>
-			<main className={classes.content}>
-				<div className={classes.toolbar} />
-				<Typography paragraph>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-					facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-				</Typography>
+		<div styleName='container'>
+			<main styleName='main-content'>
+				<div>
+					<h1>Welcome, admin</h1>
+				</div>
+				<div styleName='box-container'>
+					<div styleName='box orders-box'>
+						<img src={profitIcon} styleName='icon' />
+						<div>
+							<h4>23789</h4>Ordini
+						</div>
+					</div>
+					<div styleName='box profit-box'>
+						<img src={ordersIcon} styleName='icon' />
+						<div>
+							<h4>&euro; 12890,89</h4> Ricavo
+						</div>
+					</div>
+				</div>
 			</main>
 		</div>
 	);
 }
 
-export default Admin;
+export default CSSModules(Admin, styles, { allowMultiple: true });
