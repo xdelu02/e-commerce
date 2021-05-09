8f356c286ce48d7f3bf0cecb6de90e56b7f9dcd8
Riccardo De Luca <xdelu02@gmail.com>
2021-05-09 13:39:59 +0200

    migliorato admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,6 +4,10 @@ import styles from './Admin.module.scss';
 import Logo from '../../assets/logo/logo.png';
 import ordersIcon from '../../assets/icons/piggy-bank.png';
 import profitIcon from '../../assets/icons/check-list.png';
+import DashboardIcon from '../../assets/icons/dashboard.png';
+import ProductsIcon from '../../assets/icons/products.png';
+import OrdersIcon from '../../assets/icons/orders.png';
+import AdminIcon from '../../assets/icons/admin.png';
 import temp from '../../assets/icons/chart.png';
 
 function Admin() {
@@ -12,23 +16,36 @@ function Admin() {
 	return (
 		<div styleName='container'>
 			<ul styleName='side-menu'>
-				<li>ds</li>
-      </ul>
+				<li>
+					<img src={DashboardIcon} /> Dashboard
+				</li>
+				<li>
+					<img src={OrdersIcon} /> Ordini
+				</li>
+				<li>
+					<img src={ProductsIcon} /> Prodotti
+				</li>
+				<li>
+					<img src={AdminIcon} /> Admin
+				</li>
+			</ul>
 			<input type='checkbox' id='side-menu-btn' styleName='side-menu-btn' defaultChecked />
 			<label htmlFor='side-menu-btn' styleName='lbl-for-side-btn'></label>
 			<main styleName='main-content'>
-				<div>
-					<h1>Welcome, admin</h1>
+				<div styleName='welcome-text'>
+					<h3>
+						<span>Benvenuto,</span> admin
+					</h3>
 				</div>
 				<div styleName='box-container'>
 					<div styleName='box orders-box'>
-						<img src={profitIcon} styleName='icon' />
+						<img src={profitIcon} styleName='box-icon' />
 						<div>
 							<h4>23789</h4>Ordini
 						</div>
 					</div>
 					<div styleName='box profit-box'>
-						<img src={ordersIcon} styleName='icon' />
+						<img src={ordersIcon} styleName='box-icon' />
 						<div>
 							<h4>&euro; 12890,89</h4> Ricavo
 						</div>
