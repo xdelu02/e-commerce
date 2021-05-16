6e3ebaa6fa93bb31e8d3604d0ddc1f48f19a0135
Riccardo De Luca <xdelu02@gmail.com>
2021-05-16 02:45:14 +0200

    mmodificato admin

--- src/components/Admin/Dashboard/Dashboard.js
+++ src/components/Admin/Dashboard/Dashboard.js
@@ -1,75 +1,45 @@
-
-import React from "react";
+import React from 'react';
 import { faCashRegister, faChartLine } from '@fortawesome/free-solid-svg-icons';
 import { Col, Row } from '@themesberg/react-bootstrap';
-import { CounterWidget, CircleChartWidget, BarChartWidget, SalesValueWidget, SalesValueWidgetPhone } from "../../Widgets/Widgets";
-import { trafficShares, totalOrders } from "../../../data/charts";
+import { CounterWidget, CircleChartWidget, BarChartWidget, SalesValueWidget, SalesValueWidgetPhone } from '../../Widgets/Widgets';
+import { trafficShares, totalOrders } from '../../../data/charts';
 
-export default function Dashboard () {
-  return (
-    <>
-      <Row className="justify-content-md-center">
-        <Col xs={12} className="mb-4 d-none d-sm-block">
-          <SalesValueWidget
-            title="Sales Value"
-            value="10,567"
-            percentage={10.57}
-          />
-        </Col>
-        <Col xs={12} className="mb-4 d-sm-none">
-          <SalesValueWidgetPhone
-            title="Sales Value"
-            value="10,567"
-            percentage={10.57}
-          />
-        </Col>
-        <Col xs={12} sm={6} xl={4} className="mb-4">
-          <CounterWidget
-            category="Customers"
-            title="345k"
-            period="Feb 1 - Apr 1"
-            percentage={18.2}
-            icon={faChartLine}
-            iconColor="shape-primary"
-          />
-        </Col>
+export default function Dashboard() {
+	return (
+		<>
+			<Row className='justify-content-md-center mt-4'>
+				<Col xs={12} className='mb-4 d-none d-sm-block'>
+					<SalesValueWidget title='Vendite' value='10,567' percentage={10.57} />
+				</Col>
+				<Col xs={12} className='mb-4 d-sm-none'>
+					<SalesValueWidgetPhone title='Vendite' value='10,567' percentage={10.57} />
+				</Col>
+				<Col xs={12} sm={6} xl={4} className='mb-4'>
+					<CounterWidget category='Clienti' title='345k' period='Feb 1 - Apr 1' percentage={18.2} icon={faChartLine} iconColor='shape-primary' />
+				</Col>
 
-        <Col xs={12} sm={6} xl={4} className="mb-4">
-          <CounterWidget
-            category="Revenue"
-            title="$43,594"
-            period="Feb 1 - Apr 1"
-            percentage={28.4}
-            icon={faCashRegister}
-            iconColor="shape-tertiary"
-          />
-        </Col>
+				<Col xs={12} sm={6} xl={4} className='mb-4'>
+					<CounterWidget category='Ricavi' title='43,594' period='Feb 1 - Apr 1' percentage={28.4} icon={faCashRegister} iconColor='shape-tertiary' />
+				</Col>
 
-        <Col xs={12} sm={6} xl={4} className="mb-4">
-          <CircleChartWidget
-            title="Traffic Share"
-            data={trafficShares} />
-        </Col>
-      </Row>
+				<Col xs={12} sm={6} xl={4} className='mb-4'>
+					<CircleChartWidget title='Traffic Share' data={trafficShares} />
+				</Col>
+			</Row>
 
-      <Row>
-        <Col xs={12} xl={12} className="mb-4">
-          <Row>
-            <Col xs={12} xl={4}>
-              <Row>
-                <Col xs={12} className="mb-4">
-                  <BarChartWidget
-                    title="Total orders"
-                    value={452}
-                    percentage={18.2}
-                    data={totalOrders} />
-                </Col>
-
-              </Row>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </>
-  );
-};
+			<Row>
+				<Col xs={12} xl={12} className='mb-4'>
+					<Row>
+						<Col xs={12} xl={4}>
+							<Row>
+								<Col xs={12} className='mb-4'>
+									<BarChartWidget title='Total orders' value={452} percentage={18.2} data={totalOrders} />
+								</Col>
+							</Row>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
+		</>
+	);
+}
