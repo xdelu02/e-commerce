d3902bb4d2ece912e9a5c992f78a688419e2ff4a
Riccardo De Luca <xdelu02@gmail.com>
2021-05-17 21:45:58 +0200

    file eliminati

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -5,31 +5,9 @@ import { faAngleDown, faAngleUp, faChartArea, faChartBar, faChartLine, faFlagUsa
 import { faAngular, faBootstrap, faReact, faVuejs } from "@fortawesome/free-brands-svg-icons";
 import { Col, Row, Card, Image, Button, ListGroup, ProgressBar } from '@themesberg/react-bootstrap';
 import { CircleChart, BarChart, SalesValueChart, SalesValueChartphone } from "../Charts/Charts";
-
-import Profile1 from "../../assets/img/team/profile-picture-1.jpg";
-import ProfileCover from "../../assets/img/profile-cover.jpg";
-
 import teamMembers from "../../data/teamMembers";
 
 
-export const ProfileCardWidget = () => {
-  return (
-    <Card border="light" className="text-center p-0 mb-4">
-      <div style={{ backgroundImage: `url(${ProfileCover})` }} className="profile-cover rounded-top" />
-      <Card.Body className="pb-5">
-        <Card.Img src={Profile1} alt="Neil Portrait" className="user-avatar large-avatar rounded-circle mx-auto mt-n7 mb-4" />
-        <Card.Title>Neil Sims</Card.Title>
-        <Card.Subtitle className="fw-normal">Senior Software Engineer</Card.Subtitle>
-        <Card.Text className="text-gray mb-4">New York, USA</Card.Text>
-
-        <Button variant="primary" size="sm" className="me-2">
-          <FontAwesomeIcon icon={faUserPlus} className="me-1" /> Connect
-        </Button>
-        <Button variant="secondary" size="sm">Send Message</Button>
-      </Card.Body>
-    </Card>
-  );
-};
 
 export const ChoosePhotoWidget = (props) => {
   const { title, photo } = props;
