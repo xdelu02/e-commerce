58e9df52e505bd0951760bde6d126939e92163b7
AleFeri <50620354+AleFeri@users.noreply.github.com>
2021-05-17 20:19:42 +0200

    modify prodotti completato (no img)

--- src/components/Admin/Prodotti/ModifyProduct.js
+++ src/components/Admin/Prodotti/ModifyProduct.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import { Col, Row, Card, Form, Button, Image } from '@themesberg/react-bootstrap';
 import { useDropzone } from 'react-dropzone';
 
-export default function ModifyProduct() {
-	const id = parseInt(window.location.pathname.replace(/^\D+/g, ''), 10);
+export default function ModifyProduct(props) {
 	const [nome, setNome] = useState('');
 	const [descS, setDescS] = useState('');
 	const [descL, setDescL] = useState('');
 	const [categoria, setCategoria] = useState('');
 	const [prezzo, setPrezzo] = useState('');
 	const [qta, setQta] = useState(0);
-
-	const sendProductData = () => {
-		console.log('LAVORA QUA FERI La FUNZIONE è già linkata al battone Salva');
-	};
+	const history = useHistory('/admin/prodotti');
+	let id = props.match.params.id;
 
 	const handleNome = (e) => {
 		setNome(e.target.value);
@@ -34,6 +32,49 @@ export default function ModifyProduct() {
 		setQta(parseInt(e.target.value, 10));
 	};
 
+	const sendProductData = () => {
+		fetch('http://ecommerce.ideeinbit.it/api/prodotti/', {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				idProdotto: parseInt(id),
+				nome: nome,
+				descS: descS,
+				descL: descL,
+				prezzo: parseFloat(prezzo),
+				quantita: parseInt(qta),
+				idCategoria: categoria
+			})
+		})
+			.then((response) => response.json())
+			.then(() => {
+				history.push('/admin/prodotti');
+			});
+	};
+
+	useEffect(() => {
+		fetch('http://ecommerce.ideeinbit.it/api/prodotti/' + id)
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					if (!(typeof result.records === 'undefined' || result.records === null)) {
+						history.push('/404');
+					}
+					setNome(result.nome);
+					setDescS(result.descS);
+					setDescL(result.descL);
+					setPrezzo(result.prezzo);
+					setCategoria(result.idCategoria);
+					setQta(result.quantita);
+				},
+				(error) => {
+					history.push('/admin/prodotti');
+				}
+			);
+	}, [id, history]);
+
 	return (
 		<Card border='light' className='bg-white shadow-xs mb-4 mt-3'>
 			<Card.Body>
@@ -43,13 +84,13 @@ export default function ModifyProduct() {
 						<Col md={2} className='mb-3'>
 							<Form.Group>
 								<Form.Label>ID</Form.Label>
-								<Form.Control required type='text' readOnly />
+								<Form.Control required type='text' value={id} readOnly />
 							</Form.Group>
 						</Col>
 						<Col md={4} className='mb-3'>
 							<Form.Group>
 								<Form.Label>Nome prodotto</Form.Label>
-								<Form.Control required type='text' onChange={handleNome} />
+								<Form.Control required type='text' value={nome} onChange={handleNome} />
 							</Form.Group>
 						</Col>
 					</Row>
@@ -57,7 +98,7 @@ export default function ModifyProduct() {
 						<Col md={6} className='mb-3'>
 							<Form.Group>
 								<Form.Label>Descrizione corta</Form.Label>
-								<Form.Control required as='textarea' rows={3} onChange={handleDescS} />
+								<Form.Control required as='textarea' rows={3} value={descS} onChange={handleDescS} />
 							</Form.Group>
 						</Col>
 					</Row>
@@ -66,7 +107,7 @@ export default function ModifyProduct() {
 						<Col md={6} className='mb-3'>
 							<Form.Group>
 								<Form.Label>Descrizione lunga</Form.Label>
-								<Form.Control required as='textarea' rows={3} onChange={handleDescL} />
+								<Form.Control required as='textarea' rows={3} value={descL} onChange={handleDescL} />
 							</Form.Group>
 						</Col>
 					</Row>
@@ -75,14 +116,14 @@ export default function ModifyProduct() {
 						<Col sm={3}>
 							<Form.Group>
 								<Form.Label>Categoria</Form.Label>
-								<Form.Control required type='text' onChange={handleCategoria} />
+								<Form.Control required type='text' value={categoria} onChange={handleCategoria} />
 							</Form.Group>
 						</Col>
 
 						<Col sm={2} className='mb-3'>
 							<Form.Group>
 								<Form.Label>Prezzo</Form.Label>
-								<Form.Control required type='text' onChange={handlePrezzo} />
+								<Form.Control required type='text' value={prezzo} onChange={handlePrezzo} />
 							</Form.Group>
 						</Col>
 					</Row>
@@ -91,12 +132,12 @@ export default function ModifyProduct() {
 						<Col sm={2}>
 							<Form.Group>
 								<Form.Label>Quantità</Form.Label>
-								<Form.Control required type='number' onChange={handleQta} />
+								<Form.Control required type='number' value={qta} onChange={handleQta} />
 							</Form.Group>
 						</Col>
 					</Row>
 					<div className='mt-4'>
-						<Button variant='primary' type='submit' onClick={sendProductData}>
+						<Button variant='primary' onClick={sendProductData}>
 							Salva
 						</Button>
 					</div>
