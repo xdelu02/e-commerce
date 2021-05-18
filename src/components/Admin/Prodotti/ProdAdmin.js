2861c32488be7dd9ebef7158c06037c16525381c
riccardodeluca <xdelu02@gmail.com>
2021-05-18 13:42:05 +0200

    modificate card

--- src/components/Admin/Prodotti/ProdAdmin.js
+++ src/components/Admin/Prodotti/ProdAdmin.js
@@ -5,18 +5,19 @@ import { Link } from 'react-router-dom';
 
 function ProdAdmin({ idProdotto, nome, quantita, prezzo }) {
 	return (
-		<Link to={'/admin/prodotti/' + idProdotto} styleName='product-card' className='card'>
-			<img src={'http://ecommerce.ideeinbit.it/img/' + nome + '.png'} className='card-img-top' alt='...' />
+		<Link to={'/admin/prodotti/' + idProdotto} styleName='product-card' className='card w-100'>
+			<img src={'http://ecommerce.ideeinbit.it/img/' + nome + '.png'} className='card-img-top'  alt='...' />
 			<div className='card-body'>
 				<h5 className='card-title'>{nome}</h5>
-				<p>Quantita</p>
-				<p>{quantita}</p>
-				<p>Prezzo</p>
-				<p>{prezzo} €</p>
+				<div className="card-text">
+					<p>Quantita</p>
+					<p>{quantita}</p>
+					<p>Prezzo</p>
+					<p>{prezzo} €</p>
+				</div>
 			</div>
 		</Link>
 	);
 }
 
 export default CSSModules(ProdAdmin, styles, { allowMultiple: true });
-
