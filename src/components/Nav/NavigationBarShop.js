63377250bcab4b9e62eacef2d4b2ffb8f161df76
Riccardo De Luca <xdelu02@gmail.com>
2021-05-21 15:58:02 +0200

    navbar

--- src/components/Nav/NavigationBarShop.js
+++ src/components/Nav/NavigationBarShop.js
@@ -9,17 +9,17 @@ import { Navbar, Nav, Image } from '@themesberg/react-bootstrap';
 function NavigationBarShop() {
 	return (
 		<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark' sticky='top' styleName="navbar">
-			<Nav.Link href='/' className='ms-4'>
+			<Nav.Link href='/' className='ms-4 py-0'>
 				<img src={Logo} alt='Logo' styleName="logo"/>
 			</Nav.Link>
-			<Navbar.Toggle aria-controls='responsive-navbar-nav' className='me-4' styleName="toggler"/>
+			<Navbar.Toggle aria-controls='responsive-navbar-nav' className='me-4 py-0' styleName="toggler"/>
 			<Navbar.Collapse id='responsive-navbar-nav'>
-				<Nav.Link href='/'>Home</Nav.Link>
-				<Nav.Link href='/shop'>Shop</Nav.Link>
-				<Nav.Link href='/shop'>Account</Nav.Link>
-				<Nav.Link href='/shop'>About</Nav.Link>
+				<Nav.Link href='/' className="py-0">Home</Nav.Link>
+				<Nav.Link href='/shop' className="py-0">Shop</Nav.Link>
+				<Nav.Link href='/shop' className="py-0">Account</Nav.Link>
+				<Nav.Link href='/shop' className="py-0">About</Nav.Link>
 			</Navbar.Collapse>
-			<Nav.Link href='/carrello' className='ms-auto'>
+			<Nav.Link href='/carrello' className='ms-auto py-0'>
 				<img src={ShoppingCart} styleName='cart' />
 			</Nav.Link>
 		</Navbar>
