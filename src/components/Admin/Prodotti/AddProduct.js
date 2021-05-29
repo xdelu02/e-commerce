fb395feb2edd6a824d86c45f7d77bce977057842
De Luca Riccardo <xdelu02@gmail.com>
2021-05-29 20:02:42 +0200

    Aggiunte categorie in AddProduct

--- src/components/Admin/Prodotti/AddProduct.js
+++ src/components/Admin/Prodotti/AddProduct.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { Col, Row, Card, Form, Button, Image } from '@themesberg/react-bootstrap';
 import { useDropzone } from 'react-dropzone';
@@ -10,6 +10,7 @@ function AddProduct() {
 	const [descS, setDescS] = useState('');
 	const [descL, setDescL] = useState('');
 	const [categoria, setCategoria] = useState('');
+	const [categorie, setCategorie] = useState([]);
 	const [prezzo, setPrezzo] = useState('');
 	const [qta, setQta] = useState(0);
 	const [dropZoneClass, setDropZoneClass] = useState('');
@@ -128,6 +129,21 @@ function AddProduct() {
 		);
 	};
 
+	useEffect(() => {
+		fetch('/api/categorie/')
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					if (result.message !== 'Categorie non trovate') {
+						setCategorie(result.records);
+					}
+				},
+				(error) => {
+					console.log(error);
+				}
+			);
+	}, []);
+
 	return (
 		<Card border='light' className='bg-white shadow-xs mb-4 mt-3'>
 			<Card.Body>
@@ -150,7 +166,7 @@ function AddProduct() {
 								<Col md={10} className='mb-3'>
 									<Form.Group>
 										<Form.Label>Descrizione corta</Form.Label>
-										<Form.Control maxLength='20' required as='textarea' rows={3} onChange={handleDescS} />
+										<Form.Control maxLength='50' required as='textarea' rows={3} onChange={handleDescS} />
 									</Form.Group>
 								</Col>
 							</Row>
@@ -168,7 +184,13 @@ function AddProduct() {
 								<Col md={10} className='mb-3'>
 									<Form.Group>
 										<Form.Label>Categoria</Form.Label>
-										<Form.Control required type='text' onChange={handleCategoria} />
+										<Form.Select onChange={handleCategoria}>
+											{categorie.map((e, i) => (
+												<option value={e} key={i}>
+													{e.idCategoria}
+												</option>
+											))}
+										</Form.Select>
 									</Form.Group>
 								</Col>
 
